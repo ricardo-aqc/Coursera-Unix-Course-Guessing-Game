0359ed0cfa083cde708063d9973fb32dbe932c9a
ricardo-aqc <59422066+ricardo-aqc@users.noreply.github.com>
2022-04-18 19:35:43 -0500

    Files for exercise

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Coursera-Unix-Course-Guessing-Game
-GuessingGame Repository
+# Guessing Game Unix Exercise 
+## Guessing Game README.md
+
+ This is the README.md file needed for the exercise 
+
+ ** Date that the file was made ** Mon Apr 18 19:26:58 CDT 2022
+** The number of lines in the the sh file is:** 27
---
+++ guessinggame.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "Welcome to the guessing game program"
+echo "In this program, you will try to guess the number of"
+echo "files there are in the current directory!"
+
+function ask {
+	echo "Guess/enter the number of files in the current directory:"
+	read guess
+    filenumber=$(ls -l |grep "^-"|wc -l)
+}
+
+ask
+
+while [[ $filenumber -ne $guess ]]
+do
+	if [[ $guess -gt $filenumber ]] 
+	then
+		echo "Your guess is too high. Try again with a lower number!"
+	else
+		echo "Your guess is too low. Try again with a higher number!"
+	fi
+	ask
+done
+
+echo "You got it right!"
+echo "The number of files was $filenumber"
+ls -1
